55b565b9c2d0e564770b96f96a414a9cfb8b05fb
Brian Becker <brbecker@ucdavis.edu>
2019-01-12 23:55:29 -0800

    switch to python 3

--- dashutil/settings.py
+++ dashutil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dashutil-env.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['dashutil-env.us-west-2.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
