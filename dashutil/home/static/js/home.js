743f8b3999db7dbdce3fa90640db2cb23b1cae4e
Brian Becker <brbecker@ucdavis.edu>
2019-01-23 23:21:24 -0800

    Some refactoring, theme logo for switching

--- dashutil/home/static/js/home.js
+++ dashutil/home/static/js/home.js
@@ -1,13 +1,34 @@
 // home.js
 
+// General constants
+const generalConstants = {
+    inputTextId:        "input-text",
+    searchBoxId:        "search-box", 
+    searchIconId:       "search-icon",
+    searchButtonId:     "search-button",
+    oldSearchId:        "old-search",
+    newSearchId:        "new-search", 
+    prevClass:          "prev",
+    expandedClass:      "expanded", 
+    logoImgId:          "logo-img-id",
+    logoTextId:         "logo-text-id", 
+    themeImgId:         "theme-img-id"
+};
+
+// light theme variables
+const darkThemeConstants = {
+
+}
+var currentThemeConstants = {
+
+}
+
 $(document).ready(function() {
 
     // Set up keyboard event handler
     $(document).keydown(function(e) {
         handleKeyboardEvents(e);
     }); 
-
-    $(".dropdown-toggle").dropdown();
 }); 
 
 /*****************************************************************************
@@ -16,7 +37,7 @@ $(document).ready(function() {
 
 // Finds or creates a room as appropriate
 function findOrCreateRoom() {
-    var roomToSearchFor = document.getElementById("input-text").value.toString().trim();
+    var roomToSearchFor = document.getElementById(generalConstants.inputTextId).value.toString().trim();
 
     if (roomToSearchFor.length > 0) {
         console.log("Searching for: " + roomToSearchFor); 
@@ -51,19 +72,19 @@ function createErrorMessage(errorMessage) {
 // Handler for keyboard events
 function handleKeyboardEvents(e) {
     var keycode = e.key.toLowerCase();
-    var searchBox = document.getElementById("search-box");
+    var searchBox = document.getElementById(generalConstants.searchBoxId);
 
     switch (keycode) {
         case "enter":
-            if (!searchBox.classList.contains("expanded")) 
+            if (!searchBox.classList.contains(generalConstants.expandedClass)) 
                 expandSearchBox(); 
-            else if (searchBox.classList.contains("expanded"))
+            else if (searchBox.classList.contains(generalConstants.expandedClass))
                 findOrCreateRoom();
             break; 
 
         case "escape": 
         case "esc": 
-            if (searchBox.classList.contains("expanded")) { 
+            if (searchBox.classList.contains(generalConstants.expandedClass)) { 
                 contractSearchBox(); 
 
                 // no this isnt a mistake
@@ -73,9 +94,9 @@ function handleKeyboardEvents(e) {
 
         case "tab":
             var logoId = document.getElementById("logo-id");
-            var searchButton = document.getElementById("search-button");
+            var searchButton = document.getElementById(generalConstants.searchButtonId);
 
-            if (!searchBox.classList.contains("expanded")) {
+            if (!searchBox.classList.contains(generalConstants.expandedClass)) {
                 e.preventDefault();
                 expandSearchBox(); 
             }
@@ -86,24 +107,24 @@ function handleKeyboardEvents(e) {
 
 // Handler for expanding the search box
 function expandSearchBox() { 
-    var searchBox = document.getElementById("search-box");
-    var searchIcon = document.getElementById("search-icon");
-    var inputText = document.getElementById("input-text");
-    var searchButton = document.getElementById("search-button"); 
-    var oldSearch = document.getElementById("old-search");
+    var searchBox = document.getElementById(generalConstants.searchBoxId);
+    var searchIcon = document.getElementById(generalConstants.searchIconId);
+    var inputText = document.getElementById(generalConstants.inputTextId);
+    var searchButton = document.getElementById(generalConstants.searchButtonId); 
+    var oldSearch = document.getElementById(generalConstants.oldSearchId);
 
-    if (searchBox.classList.contains("prev")) {
-        searchBox.classList.remove("prev");
-        searchIcon.classList.remove("prev");
-        inputText.classList.remove("prev");
-        searchButton.classList.remove("prev");
+    if (searchBox.classList.contains(generalConstants.prevClass)) {
+        searchBox.classList.remove(generalConstants.prevClass);
+        searchIcon.classList.remove(generalConstants.prevClass);
+        inputText.classList.remove(generalConstants.prevClass);
+        searchButton.classList.remove(generalConstants.prevClass);
     }
-    else if (!searchBox.classList.contains("expanded")) {
-        searchBox.classList.add("expanded");
-        searchIcon.classList.add("expanded");
-        inputText.classList.add("expanded");
-        searchButton.classList.add("expanded"); 
-        oldSearch.id = "new-search"; 
+    else if (!searchBox.classList.contains(generalConstants.expandedClass)) {
+        searchBox.classList.add(generalConstants.expandedClass);
+        searchIcon.classList.add(generalConstants.expandedClass);
+        inputText.classList.add(generalConstants.expandedClass);
+        searchButton.classList.add(generalConstants.expandedClass); 
+        oldSearch.id = generalConstants.newSearchId; 
 
         moveCaretToEnd(inputText);
     }
@@ -112,39 +133,59 @@ function expandSearchBox() {
 
 // Handler for contracting the search box
 function contractSearchBox() {
-    var searchBox = document.getElementById("search-box");
-    var searchIcon = document.getElementById("search-icon");
-    var inputText = document.getElementById("input-text");
-    var searchButton = document.getElementById("search-button"); 
-    var newSearch = document.getElementById("new-search");
+    var searchBox = document.getElementById(generalConstants.searchBoxId);
+    var searchIcon = document.getElementById(generalConstants.searchIconId);
+    var inputText = document.getElementById(generalConstants.inputTextId);
+    var searchButton = document.getElementById(generalConstants.searchButtonId); 
+    var newSearch = document.getElementById(generalConstants.newSearchId);
 
-    if (searchBox.classList.contains("expanded")) {
-        searchBox.classList.remove("expanded");
-        searchIcon.classList.remove("expanded");
-        inputText.classList.remove("expanded");
-        searchButton.classList.remove("expanded"); 
+    if (searchBox.classList.contains(generalConstants.expandedClass)) {
+        searchBox.classList.remove(generalConstants.expandedClass);
+        searchIcon.classList.remove(generalConstants.expandedClass);
+        inputText.classList.remove(generalConstants.expandedClass);
+        searchButton.classList.remove(generalConstants.expandedClass); 
 
-        searchBox.classList.add("prev");
-        searchIcon.classList.add("prev");
-        inputText.classList.add("prev");
-        searchButton.classList.add("prev"); 
-        newSearch.id = "old-search"; 
+        searchBox.classList.add(generalConstants.prevClass);
+        searchIcon.classList.add(generalConstants.prevClass);
+        inputText.classList.add(generalConstants.prevClass);
+        searchButton.classList.add(generalConstants.prevClass); 
+        newSearch.id = generalConstants.oldSearchId; 
     }      
 }
 
 
 // hover function for logo
 function hoverLogo() {
-    document.getElementById("logo-id").setAttribute("src", "/static/img/logo-dark-orange.png");
-    document.getElementById("logo-text-id").style.color = "#d6d6d6";
+    document.getElementById(generalConstants.logoImgId).setAttribute("src", "/static/img/logo-dark-highlight.png");
+    document.getElementById(generalConstants.logoTextId).style.color = "#d6d6d6";
 }
 
 
 // unhover function for logo
 function unhoverLogo() {
-    document.getElementById("logo-id").setAttribute("src", "/static/img/logo-light-orange.png");
-    document.getElementById("logo-text-id").style.color = "white";
+    document.getElementById(generalConstants.logoImgId).setAttribute("src", "/static/img/logo-dark.png");
+    document.getElementById(generalConstants.logoTextId).style.color = "white";
 }
+
+
+// hover function for logo
+function hoverTheme() {
+    document.getElementById(generalConstants.themeImgId).setAttribute("src", "/static/img/theme-light.png");
+}
+
+
+// unhover function for logo
+function unhoverTheme() {
+    document.getElementById(generalConstants.themeImgId).setAttribute("src", "/static/img/theme-dark.png");
+}
+
+
+// switch themes
+function switchThemes() {
+    document.getElementById(generalConstants.themeImgId).setAttribute("src", "/static/img/theme-dark.png");
+}
+
+
 
 /*****************************************************************************
  * Handler Functions ----- END ------
