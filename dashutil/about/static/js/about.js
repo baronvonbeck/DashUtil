cecd49d873ac3c718204129867dfface2125901a
Brian Becker <brbecker@ucdavis.edu>
2019-08-25 00:21:51 -0700

    setting up about pages

---
+++ dashutil/about/static/js/about.js
@@ -0,0 +1,28 @@
+// about.js -- initializer
+'use strict';
+
+
+$(document).ready(function() {
+    NAVBAR_EVENT_HANDLERS.addNavbarEventListeners();
+
+    // NAVBAR_CONSTANTS.themeTogglerEl.addEventListener(
+    //     "click", function(e) { 
+    //         SINGLE_EVENT_HANDLERS.switchThemesForIcons(); 
+    //     }, false);
+
+    // NAVBAR_CONSTANTS.themeTogglerEl.addEventListener(
+    //     "keydown", function(e) { 
+    //         var keycode = e.key.toLowerCase();
+
+    //         if (keycode == "enter") {
+    //             e.preventDefault();
+    //             SINGLE_EVENT_HANDLERS.switchThemesForIcons();
+    //         }
+    //     }, false);
+}); 
+
+
+
+
+
+
