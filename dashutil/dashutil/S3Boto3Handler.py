b114bc179f2ac30b845f159c53ce834763a940e0
Brian Becker <brbecker@ucdavis.edu>
2019-09-20 23:05:14 -0700

    Remove logging of uploads

--- dashutil/dashutil/S3Boto3Handler.py
+++ dashutil/dashutil/S3Boto3Handler.py
@@ -65,8 +65,8 @@ class ProgressPercentage(object):
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = (self._seen_so_far / self._size) * 100
-            sys.stdout.write(
-                "\r%s  %s / %s  (%.2f%%)" % (
-                    self._filename, self._seen_so_far, self._size,
-                    percentage))
-            sys.stdout.flush()
+            # sys.stdout.write(
+            #     "\r%s  %s / %s  (%.2f%%)" % (
+            #         self._filename, self._seen_so_far, self._size,
+            #         percentage))
+            # sys.stdout.flush()
